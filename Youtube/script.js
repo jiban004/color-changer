97fbff0e3985b29980471b364094e7de7aa869e8
Jiban Jena <149889183+jiban004@users.noreply.github.com>
2024-06-21 22:03:48 +0530

    Add files via upload

---
+++ Youtube/script.js
@@ -0,0 +1,23 @@
+const button = document.querySelectorAll('.box')
+const body = document.querySelector("body")
+
+button.forEach(function(box){
+    box.addEventListener('click',function(color){
+        if (color.target.id === 'box1'){
+            body.style.backgroundColor = 'orange'
+        }
+        if (color.target.id === 'box2'){
+            body.style.backgroundColor = 'pink'
+        }
+        if (color.target.id === 'box3'){
+            body.style.backgroundColor = 'blueviolet'
+        }
+        if (color.target.id === 'box4'){
+            body.style.backgroundColor = 'aqua'
+        }
+        if (color.target.id === 'box5'){
+            body.style.backgroundColor = 'yellow'
+        }
+
+    })
+})
